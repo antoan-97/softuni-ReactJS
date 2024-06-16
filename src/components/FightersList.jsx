f874409b40d0abe0b05969528a6998406bc863b6
antoan-97 <antoan.todorov@abv.bg>
2024-06-16 13:50:53 +0300

    create fighterList component

---
+++ src/components/FightersList.jsx
@@ -0,0 +1,12 @@
+export default function FightersList(props) {
+    return (
+        <div className="fighters-list">
+            <h2></h2>
+            <ul>
+                <li>First Fighter</li>
+                <li>Second Fighter</li>
+                <li>Third Fighter</li>
+            </ul>
+        </div>
+    );
+}
