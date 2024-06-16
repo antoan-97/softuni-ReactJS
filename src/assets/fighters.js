11167a732e7da0362b5d82fc6a18f1cb717da2f2
antoan-97 <antoan.todorov@abv.bg>
2024-06-16 14:22:51 +0300

    add more info to fightersList

--- src/assets/fighters.js
+++ src/assets/fighters.js
@@ -2,13 +2,19 @@ export default [
     {
       title: 'Conor McGregor',
       description: 'Striker',
+      wins: '22',
+      years: '35'
     },
     {
       title: 'Alex Pereira',
       description: 'Kickboxer',
+      wins: '10',
+      years: '36'
     },
     {
       title: 'Khamzat Chimaev',
       description: 'Wrestler',
+      wins: 13,
+      years: '30'
     },
   ];
