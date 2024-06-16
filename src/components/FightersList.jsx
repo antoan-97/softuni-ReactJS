09547cd3173eaeda9fa375e1b79d9a5d18684bda
antoan-97 <antoan.todorov@abv.bg>
2024-06-16 14:51:21 +0300

    render all fighters info

--- src/components/FightersList.jsx
+++ src/components/FightersList.jsx
@@ -7,6 +7,8 @@ export default function FightersList(props) {
             <h2>{props.headingText}</h2>
             <ul>
                 <li><Fighter data = {props.fighters[0]} /></li>
+                <li><Fighter data = {props.fighters[1]} /></li>
+                <li><Fighter data = {props.fighters[2]} /></li>
             </ul>
         </div>
     );
