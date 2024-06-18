c51dd755d80dcbe9bdc516b5f1d51ad28618550e
antoan-97 <antoan.todorov@abv.bg>
2024-06-18 19:58:22 +0300

    add logout to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,9 +5,10 @@ export default function Navbar(props) {
         <nav className={styles.navbar}>
             <ul className={styles['nav-menu']}>
                 <li><a href="/">Home</a></li>
+                <li><a href="/collection">Collection</a></li>
                 <li><a href="/register">Register</a></li>
                 <li><a href="/login">Login</a></li>
-                <li><a href="/collection">Collection</a></li>
+                <li><a href="/logout">Logout</a></li>
             </ul>
         </nav>
     );
