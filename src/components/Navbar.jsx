22bcddd887a825291586917b32b0c4281ca3a9e6
antoan-97 <antoan.todorov@abv.bg>
2024-06-18 19:49:28 +0300

    add some css to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,9 @@
+import styles from '../styles/Navbar.module.css'
+
 export default function Navbar(props) {
     return(
-        <nav>
-            <ul>
+        <nav className={styles.navbar}>
+            <ul className={styles['nav-menu']}>
                 <li><a href="/">Home</a></li>
                 <li><a href="/register">Register</a></li>
                 <li><a href="/login">Login</a></li>
