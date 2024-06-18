6b702605cbd68268d568e8ed2c062f7120e59a9c
antoan-97 <antoan.todorov@abv.bg>
2024-06-18 15:51:00 +0300

    create fighter list css file

--- src/components/FightersList.jsx
+++ src/components/FightersList.jsx
@@ -1,11 +1,14 @@
 import Fighter from "./Fighter";
 
+import styles from '../styles/FightersList.module.css';
+
+
 
 export default function FightersList(props) {
     return (
         <div className="fighters-list">
             <h2>{props.headingText}</h2>
-            <ul>
+            <ul className={styles.fighterList}>
                 <li><Fighter data = {props.fighters[0]} /></li>
                 <li><Fighter data = {props.fighters[1]} /></li>
                 <li><Fighter data = {props.fighters[2]} /></li>
