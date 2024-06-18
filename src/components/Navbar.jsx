7f1d465bcee80ab023b637bf09a2eb14c585ee42
antoan-97 <antoan.todorov@abv.bg>
2024-06-18 19:41:13 +0300

    create Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+export default function Navbar(props) {
+    return(
+        <nav>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/register">Register</a></li>
+                <li><a href="/login">Login</a></li>
+                <li><a href="/collection">Collection</a></li>
+            </ul>
+        </nav>
+    );
+}
