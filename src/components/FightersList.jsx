68129d324c94fd31c060678461153bb821cfc1bf
antoan-97 <antoan.todorov@abv.bg>
2024-06-18 16:20:29 +0300

    map li elements

--- src/components/FightersList.jsx
+++ src/components/FightersList.jsx
@@ -1,5 +1,5 @@
 import Fighter from "./Fighter";
-
+import fighters from "../assets/fighters";
 import styles from '../styles/FightersList.module.css';
 
 
@@ -9,9 +9,12 @@ export default function FightersList(props) {
         <div className="fighters-list">
             <h2>{props.headingText}</h2>
             <ul className={styles.fighterList}>
-                <li><Fighter data = {props.fighters[0]} /></li>
-                <li><Fighter data = {props.fighters[1]} /></li>
-                <li><Fighter data = {props.fighters[2]} /></li>
+                {fighters.map((fighter, index) =>(
+                <li key={index} className={styles.fighterItem}>  
+                <Fighter data={fighter} />
+                </li>
+                ))}
+
             </ul>
         </div>
     );
