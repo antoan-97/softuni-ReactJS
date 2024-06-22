6e5fea12141aa7a480a46cdd9868544a173ca52c
antoan-97 <antoan.todorov@abv.bg>
2024-06-22 11:54:57 +0300

    Add more images

--- src/assets/fighters.js
+++ src/assets/fighters.js
@@ -11,11 +11,13 @@ export default [
       description: 'Kickboxer',
       wins: '10',
       years: '36',
+      image: 'images/Alex.jpg'
     },
     {
       title: 'Khamzat Chimaev',
       description: 'Wrestler',
       wins: 13,
       years: '30',
+      image: 'images/Khamzatx.jpg'
     },
   ];
